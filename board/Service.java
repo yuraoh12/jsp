22e5a2da1cc13622f6c793b83bdab4e461ab6b96
OH YURA <102283560+yuraoh12@users.noreply.github.com>
2022-09-09 22:18:31 +0900

    Delete board directory

--- board/Service.java
+++
@@ -1,5 +0,0 @@
-package com.sbs.board;
-
-public class Service {
-
-}
--- board/article/dao/Dao.java
+++
@@ -1,7 +0,0 @@
-package com.sbs.board.article.dao;
-
-import java.util.List;
-
-public interface Dao {
-	List<Article> getAllAritcles();
-}
