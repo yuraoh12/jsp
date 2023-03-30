3c61d6fa1432598a69c39c68111b82f68ba01a10
OH YURA <102283560+yuraoh12@users.noreply.github.com>
2023-03-30 21:43:20 +0900

    Delete ll2 directory

--- ll2/src/main/java/com/practice/exam/demo/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cjh.exam.demo.repository;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberRepository {
-	public void doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String eamil);
-}
